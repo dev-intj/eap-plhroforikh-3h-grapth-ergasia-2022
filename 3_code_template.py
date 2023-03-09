4114f41fb04dc5fb5a70ec38388d8b8f33961760
Christos <demataschristiano@gmail.com>
2023-03-09 15:24:41 +0200

    added tasks 3 and 4

--- 3_code_template.py
+++ 3_code_template.py
@@ -1,5 +1,6 @@
 import turtle as tr
 
+
 class Pen(tr.Turtle):
     # κλάση πέννα
     def __init__(self):
@@ -7,32 +8,37 @@ class Pen(tr.Turtle):
         self.speed("fastest")
         self.ondrag(self.draw)
         self.traces = []
-        # συμπληρώσετε ένα ή περισσότερα αντικείμενα τύπου Tracer (ιχνηλάτες) στη γραφίδα Pen
-        # ...
-        # ...
+        # συμπληρώσετε ένα ή περισσότερα αντικείμενα τύπου Tracer (ιχνηλάτες)
+        # στη γραφίδα Pen
+        self.traces.append(Tracer(-1, -1, 0))
+        self.traces.append(Tracer(-1, -1, 200, 0, 1, 'blue'))
+        self.traces.append(Tracer(-1, -1, -200, 0, 1, 'green'))
 
-        
-    def draw(self, x,y):
+    def draw(self, x, y):
         self.ondrag(None)
-        self.goto(x,y)
-        # προσθέστε εδώ κώδικα, ώστε η κίνηση της γραφίδας από τον χρήστη, να κινεί και τους ιχνηλάτες 
-        # ...
-        # ...
+        self.goto(x, y)
+        for i in range(len(self.traces)):
+            self.traces[i].moveto(x, y)
+        # προσθέστε εδώ κώδικα, ώστε η κίνηση της γραφίδας από τον χρήστη,
+        # να κινεί και τους ιχνηλάτες
         self.ondrag(self.draw)
-        
+
+
 class Tracer(tr.Turtle):
     # κλάση ιχνηλάτης
     def __init__(self, dx, dy, a=0, b=0, width=1, color='red'):
-       tr.Turtle.__init__(self)
-       self.hideturtle()
-       self.color(color)
-       self.width(width)
-       self.a = a
-       self.b = b
-       self.dx = dx
-       self.dy = dy
-    def moveto(self, x,y):
+        tr.Turtle.__init__(self)
+        self.hideturtle()
+        self.color(color)
+        self.width(width)
+        self.a = a
+        self.b = b
+        self.dx = dx
+        self.dy = dy
+
+    def moveto(self, x, y):
         self.goto(x * self.dx + self.a, y * self.dy + self.b)
+
 
 p = Pen()
 tr.mainloop()
--- 4_code_template.py
+++ 4_code_template.py
@@ -1,59 +1,86 @@
-# 
-#                               --->  Fuel Pump 
-#       Ignition -->  Battery --                   
-#                               --->  Engine  
+#
+#                               --->  Fuel Pump
+#       Ignition -->  Battery --
+#                               --->  Engine
 #
 class Component:
-    def __init__(self,name):
-        # Αρχικοποίηση ονόματος, κατάστασης και δομής αποθήκευσης συνδεδεμένων εξαρτημάτων
-        pass
-    
+    # Αρχικοποίηση ονόματος, κατάστασης και δομής αποθήκευσης συνδεδεμένων εξαρτημάτων
+    def __init__(self, name):
+        self.name = name
+        self.power = False
+        self.connected = []
+
+    # Προσθήκη εξαρτήματος στη δομή αποθήκευσης συνδεδεμένων εξαρτημάτων
     def connect(self, other):
-        # Προσθήκη εξαρτήματος στη δομή αποθήκευσης συνδεδεμένων εξαρτημάτων
-        pass
-    
+        self.connected.append(other)
+
+    # Αλλαγή κατάστασης, εκκίνηση συνδεδεμένων εξαρτημάτων και εκτύπωση μηνύματος έναρξης του κάθε εξαρτήματος
     def start(self):
-        # Αλλαγή κατάστασης, εκκίνηση συνδεδεμένων εξαρτημάτων και εκτύπωση μηνύματος έναρξης του κάθε εξαρτήματος
-        pass 
+        if not self.power:
+            self.power = True
+            print(f"Εξάρτημα {self.name}: έναρξη λειτουργίας")
+            for comp in self.connected:
+                comp.start()
 
+    # Αναστολή λειτουργίας συνδεδεμένων εξαρτημάτων, αλλαγή κατάστασης και εκτύπωση μηνύματος αναστολής του εξαρτήματος
     def stop(self):
-        # Αναστολή λειτουργίας συνδεδεμένων εξαρτημάτων, αλλαγή κατάστασης και εκτύπωση μηνύματος αναστολής του εξαρτήματος
-        pass 
+        if self.power:
+            for comp in self.connected:
+                comp.stop()
+            self.power = False
+            print(f"Εξάρτημα {self.name}: παύση λειτουργίας")
 
+    # Εκτύπωση συνδεδεμένων εξαρτημάτων και κατάστασης εξαρτήματος
     def status(self):
-        # Εκτύπωση συνδεδεμένων εξαρτημάτων και κατάστασης εξαρτήματος 
-        pass 
+        if (self.power):
+            print(f"Κατάσταση: {self.name} σε λειτουργία")
+        else:
+            print(f"Κατάσταση: {self.name} εκτός λειτουργίας")
 
-class Ignition(Component):  
-    def __init__(self):
-        pass
+        for component in self.connected:
+            component.status()
+
+
+class Ignition(Component):
+    def __init__(self, name):
+        super().__init__(name)
+
 
 class Battery(Component):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        super().__init__(name)
 
-class FuelPump(Component):  
-    def __init__(self):
-        pass
+
+class FuelPump(Component):
+    def __init__(self, name):
+        super().__init__(name)
+
 
 class Engine(Component):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        super().__init__(name)
+
 
 class Car:
     def __init__(self):
         # Δημιουργία των εξαρτημάτων του αυτοκινήτου
-        pass
+        self.ignition = Ignition('Μίζα')
+        self.battery = Battery('Μπαταρία')
+        self.fuelPump = FuelPump('Αντλία Καυσίμου')
+        self.engine = Engine('Κινητήρας')
 
         # Σύνδεση των εξαρτημάτων μεταξύ τους
-        pass
+        self.ignition.connect(self.battery)
+        self.battery.connect(self.fuelPump)
+        self.battery.connect(self.engine)
 
     def status(self):
         print('=========================')
         self.ignition.status()
         print('=========================')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     car = Car()
     car.status()
     car.ignition.start()
